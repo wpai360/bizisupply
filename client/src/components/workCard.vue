f28383c7ecc9118d9306066ff6b208224cdd4699
seamas zhou <seamaszhou@gmail.com>
2019-10-15 16:26:47 +1000

    code fix

--- client/src/components/workCard.vue
+++ client/src/components/workCard.vue
@@ -5,127 +5,44 @@
       deck
     >
       <b-card
+        v-for="(card, index) in cardsOne"
+        :key="index"
+        :ref="`card_${index}`"
         text-variant="black"
         class="text-center"
       >
         <div>
           <h4>
             <b-img
-              :src="require('../assets/how-it-works/service0.png')"
+              :src="card.image"
               alt="image"
             />
           </h4>
         </div>
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-      </b-card>
-
-      <b-card
-        text-variant="black"
-        class="text-center"
-      >
-        <div>
-          <h4>
-            <b-img
-              :src="require('../assets/how-it-works/service0.png')"
-              alt="image"
-            />
-          </h4>
-        </div>
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-      </b-card>
-
-      <b-card
-        text-variant="black"
-        class="text-center"
-      >
-        <div>
-          <h4>
-            <b-img
-              :src="require('../assets/how-it-works/service0.png')"
-              alt="image"
-            />
-          </h4>
-        </div>
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-      </b-card>
-
-      <b-card
-        text-variant="black"
-        class="text-center"
-      >
-        <div>
-          <h4>
-            <b-img
-              :src="require('../assets/how-it-works/service0.png')"
-              alt="image"
-            />
-          </h4>
-        </div>
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+        <b-card-text>{{ card.text }}</b-card-text>
       </b-card>
     </b-card-group>
+
     <b-card-group
       class="mt-5"
       deck
     >
       <b-card
+        v-for="(card, index) in cardsTwo"
+        :key="index"
+        :ref="`card_${index}`"
         text-variant="black"
         class="text-center"
       >
         <div>
           <h4>
             <b-img
-              :src="require('../assets/how-it-works/service0.png')"
+              :src="card.image"
               alt="image"
             />
           </h4>
         </div>
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-      </b-card>
-
-      <b-card
-        text-variant="black"
-        class="text-center"
-      >
-        <div>
-          <h4>
-            <b-img
-              :src="require('../assets/how-it-works/service0.png')"
-              alt="image"
-            />
-          </h4>
-        </div>
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-      </b-card>
-
-      <b-card
-        text-variant="black"
-        class="text-center"
-      >
-        <div>
-          <h4>
-            <b-img
-              :src="require('../assets/how-it-works/service0.png')"
-              alt="image"
-            />
-          </h4>
-        </div>
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
-      </b-card>
-
-      <b-card
-        text-variant="black"
-        class="text-center"
-      >
-        <div>
-          <h4>
-            <b-img
-              :src="require('../assets/how-it-works/service0.png')"
-              alt="image"
-            />
-          </h4>
-        </div>
-        <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+        <b-card-text>{{ card.text }}</b-card-text>
       </b-card>
     </b-card-group>
   </div>
@@ -138,5 +55,23 @@
 </style>
 
 <script>
-export default {};
+export default {
+  name: "Card",
+  data() {
+    return {
+      cardsOne: [
+        { title: "gallery", text: "text1", image: require('../assets/how-it-works/service0.png') },
+        { title: "gallery", text: "text2", image: require('../assets/how-it-works/service1.png') },
+        { title: "gallery", text: "text3", image: require('../assets/how-it-works/service2.png') },
+        { title: "gallery", text: "text4", image: require('../assets/how-it-works/service3.png') }
+      ],
+      cardsTwo: [
+        { title: "gallery", text: "text5", image: require('../assets/how-it-works/service4.png') },
+        { title: "gallery", text: "text6", image: require('../assets/how-it-works/service5.png') },
+        { title: "gallery", text: "text7", image: require('../assets/how-it-works/service6.png') },
+        { title: "gallery", text: "text8", image: require('../assets/how-it-works/service7.png') }
+      ]
+    };
+  }
+};
 </script>
