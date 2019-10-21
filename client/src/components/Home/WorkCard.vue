6a91c7cf0614906a87aa85398672dfd0f433cb74
seamas zhou <seamaszhou@gmail.com>
2019-10-21 12:02:34 +1000

    changed words

--- client/src/components/Home/WorkCard.vue
+++ client/src/components/Home/WorkCard.vue
@@ -19,7 +19,7 @@
             />{{ index +1 }}. {{ card.title }}
           </p>
         </div>
-        <b-card-text>{{ card.text }}</b-card-text>
+        <b-card-text><ul><li v-html="card.text"></li></ul></b-card-text>
       </b-card>
     </b-card-group>
 
@@ -42,7 +42,7 @@
             />{{ index +5 }}. {{ card.title }}
           </p>
         </div>
-        <b-card-text>{{ card.text }}</b-card-text>
+        <b-card-text><ul><li v-html="card.text"></li></ul></b-card-text>
       </b-card>
     </b-card-group>
   </div>
@@ -60,16 +60,16 @@ export default {
   data() {
     return {
       cardsOne: [
-        { title: "Create Your Master List", text: "", image: require('../../assets/how-it-works/service0.png') },
-        { title: "Tell Us What You Want", text: "", image: require('../../assets/how-it-works/service1.png') },
-        { title: "Suppliers Quote", text: "", image: require('../../assets/how-it-works/service2.png') },
-        { title: "Buyer Receives The Supplier's Offer", text: "", image: require('../../assets/how-it-works/service3.png') }
+        { title: "Create Your Master List", text: "Text", image: require('../../assets/how-it-works/service0.png') },
+        { title: "Tell Us What You Want", text: "Text", image: require('../../assets/how-it-works/service1.png') },
+        { title: "Suppliers Quote", text: "Text", image: require('../../assets/how-it-works/service2.png') },
+        { title: "Buyer Receives The Supplier's Offer", text: "Text", image: require('../../assets/how-it-works/service3.png') }
       ],
       cardsTwo: [
-        { title: "Buyer's Options Are", text: "", image: require('../../assets/how-it-works/service4.png') },
-        { title: "Supplier Confirmation", text: "", image: require('../../assets/how-it-works/service5.png') },
-        { title: "Pay And Delivery", text: "", image: require('../../assets/how-it-works/service6.png') },
-        { title: "Rate The Deal", text: "", image: require('../../assets/how-it-works/service7.png') }
+        { title: "Buyer's Options Are", text: "Text", image: require('../../assets/how-it-works/service4.png') },
+        { title: "Supplier Confirmation", text: "Text", image: require('../../assets/how-it-works/service5.png') },
+        { title: "Pay And Delivery", text: "Text", image: require('../../assets/how-it-works/service6.png') },
+        { title: "Rate The Deal", text: "Text", image: require('../../assets/how-it-works/service7.png') }
       ]
     };
   }
