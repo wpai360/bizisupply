b99bba7f7aa831b4753b58b9685eb6f904a1fd4a
seamas zhou <seamaszhou@gmail.com>
2020-01-16 12:33:50 +1000

    bug fix

---
+++ application/controllers/api.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+class Api extends Rest_Controller {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+        $this->load->model('BuyerOrderDashboardModel');
+    }
+
+    public function users_get($id=0)
+    {
+//        $data = $this->BuyerOrderDashboardModel->viewOrder(intval($id));
+//        $this->response($data, 200);
+        
+    }
+}
