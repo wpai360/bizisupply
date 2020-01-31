e891b58f92aa4df37b9cc8059154119511095f7b
seamas zhou <seamaszhou@gmail.com>
2020-01-31 14:14:56 +1000

    fixed composer directory

--- application/config/config.php
+++ application/config/config.php
@@ -142,8 +142,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
-
+$config['composer_autoload'] = realpath(APPPATH . '../vendor/autoload.php');
 /*
 |--------------------------------------------------------------------------
 | Allowed URL Characters
@@ -454,7 +453,7 @@ $config['global_xss_filtering'] = TRUE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = TRUE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
