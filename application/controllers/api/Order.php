982fd2857860f1dbe6a70ae9d127ca1bbf6b22e8
seamas zhou <seamaszhou@gmail.com>
2020-03-02 13:02:00 +1000

    code clean

--- application/controllers/api/Order.php
+++ application/controllers/api/Order.php
@@ -1,31 +1,30 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 require APPPATH . 'libraries/REST_Controller.php';
-class Order extends Rest_Controller {
+class Order extends Rest_Controller
+{
 
-    function __construct()
-    {
-        // Construct the parent class
-        parent::__construct();
-        $this->load->model('BuyerOrderDashboardModel');
-    }
+  function __construct()
+  {
 
-    public function orders_get($id=0)
-    {
-        $data = $this->BuyerOrderDashboardModel->viewOrder(intval($id));
-        $this->response($data, 200);
-        
-    }
+    parent::__construct();
+    $this->load->model('BuyerOrderDashboardModel');
+  }
 
-    public function update_put($order_id=0, $user_id=0)
-    {
-        $data = $this->BuyerOrderDashboardModel->UpdateOrderRequest(intval($order_id), intval($user_id));
-        if($data == 1){
-          $this->response(['status' => TRUE, 'message' => 'Order has been cancelled', 'data'=>$data], REST_Controller::HTTP_OK);
-        }else{
-          $this->response(['status' => FALSE, 'message' => 'Order has not been cancelled', 'data'=>$data], REST_Controller::HTTP_BAD_REQUEST);
-        }
+  public function orders_get($id = 0)
+  {
+    $data = $this->BuyerOrderDashboardModel->viewOrder(intval($id));
+    $this->response($data, 200);
+  }
+
+  public function update_put($order_id = 0, $user_id = 0)
+  {
+    $data = $this->BuyerOrderDashboardModel->UpdateOrderRequest(intval($order_id), intval($user_id));
+    if ($data == 1) {
+      $this->response(['status' => TRUE, 'message' => 'Order has been cancelled', 'data' => $data], REST_Controller::HTTP_OK);
+    } else {
+      $this->response(['status' => FALSE, 'message' => 'Order has not been cancelled', 'data' => $data], REST_Controller::HTTP_BAD_REQUEST);
     }
+  }
 }
-
